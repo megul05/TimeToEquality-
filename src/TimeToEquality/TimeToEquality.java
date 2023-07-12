ab22ec32c69f3cfbb075af946d8d08cf20b68cbe
Megul Raj S <129952976+megul05@users.noreply.github.com>
2023-07-12 06:39:10 -0700

    Update TimeToEquality.java

--- src/TimeToEquality/TimeToEquality.java
+++ src/TimeToEquality/TimeToEquality.java
@@ -2,8 +2,34 @@ package TimeToEquality;
 
 public class TimeToEquality {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static int minimumTime (int array[]){
+        int maxElement = Integer.MIN_VALUE;
+        for (int elements : array){
+            maxElement = Math.max(maxElement, elements);
+        }
+        int time = 0;
+        for (int elements : array){
+            time += (maxElement - elements);
+        }
+        return time;
+    }
+
+    public static void main(String[] args) {
+    	Scanner scan = new Scanner(System.in); 
+		System.out.println("Enter Size of the Array :");
+		int number = scan.nextInt();
+		System.out.println("Enter elements of the Array :");
+		int max =Integer.MIN_VALUE;
+		int[] arr = new int[number];
+		for(int i =0;i<number;i++)
+		{
+			arr[i]=scan.nextInt();
+			if(max < arr[i])
+			{
+				max = arr[i];
+			}
+		}	
+        System.out.println(minimumTime(arr));
 
 	}
 
